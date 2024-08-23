3663f17c9ad1a2f9cac374a5791fdbb433d51cf2
Martin-rosillo <158523514+Martin-rosillo@users.noreply.github.com>
2024-08-22 21:58:26 -0600

    Ejercicios.py

---
+++ Ejercicios.py
@@ -0,0 +1,42 @@
+# Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+
+#mayor = 18
+#edad = int(input("Ingrese su edad:"))
+#if edad >= mayor:
+#    print("Eres mayor de edad")
+#else:
+#    print("saquese a chingar a su madre")
+
+#Escribir un programa que almacene la cadena de caracteres contraseña en una variable, pregunte al usuario por la contraseña e imprima por pantalla si la contraseña introducida por el usuario coincide con la guardada en la variable sin tener en cuenta mayúsculas y minúsculas.
+
+#Pass = str(input("Ingrese nueva contraseña: "))
+#Pass.lower
+#Try = str(input("Verifique la contraseña: "))
+#Try.lower
+#if Try == Pass:
+#    print("La password se creo correctamente.")
+#else:
+#    print ("La password es incorrecta")
+
+#Escribir un programa que pida al usuario dos números y muestre por pantalla su división. Si el divisor es cero el programa debe mostrar un error.
+
+#num1 = int(input("Ingresa el primer numero: "))
+#num2 = int(input("Ingresa el segundo numero: "))
+#resultado = num1 % num2
+#print (num1 / num2)
+#if resultado == 0:
+#    print ("Error")
+#else:
+#    print("Residuo",resultado)
+
+# Escribir un programa que pida al usuario un número entero y muestre por pantalla si es par o impar.
+
+#num1 = float(input("Ingresa un numero: "))
+#par = num1 % 2
+#print (par)
+#if par == 0:
+#    print ("Es par")
+#else:
+#    print ("No es par")
+
+# Para tributar un determinado impuesto se debe ser mayor de 16 años y tener unos ingresos iguales o superiores a 1000 € mensuales. Escribir un programa que pregunte al usuario su edad y sus ingresos mensuales y muestre por pantalla si el usuario tiene que tributar o no.
